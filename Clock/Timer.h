c51cd2efba0ef81ed0cb441f16afbb1b277607d6
nimodung <53554746+nimodung@users.noreply.github.com>
2019-09-09 15:45:17 +0900

    Update Timer.h

--- Clock/Timer.h
+++ Clock/Timer.h
@@ -2,7 +2,7 @@
  * Timer.h
  *
  * Created: 2019-04-15 오후 3:36:06
- *  Author: user
+ *  Author: Kim Hee Ram
  */ 
 
 
@@ -10,8 +10,6 @@
 #define TIMER_H_
 
 void Timer0_init(void);
-void Timer0_HC_SR04_init(void);
 void Timer1_init_CTC_outA(void);
-void Timer0_init_CTC_outA(void);
 
 #endif /* TIMER_H_ */
