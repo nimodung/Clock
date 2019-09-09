b3133a3e4ace1b7efb20ff7eeef01cde3b365012
nimodung <53554746+nimodung@users.noreply.github.com>
2019-09-09 15:43:18 +0900

    Update Speaker.c

--- Clock/Speaker.c
+++ Clock/Speaker.c
@@ -65,48 +65,7 @@ int husuabi_bell_beat[] = {
 	 BEAT_1_4, BEAT_1_4, BEAT_1_8, BEAT_1_8, BEAT_1_8, BEAT_1_8, BEAT_1_4, BEAT_1_4, BEAT_1_4, BEAT_1_4
 };
 
-int Speakr_main()
-{
-	Timer1_init_CTC_outA();	
-	//Timer0_init_CTC_outA();
-while(1)
-	{
-		
-		//Music_player(school_bell_tune, school_bell_beat);
-		//Music_player(san_rabit_bell_tune, san_rabit_bell_beat);
-		Music_player(san_rabit_bell_tune, san_rabit_bell_beat_16);
-		//Music_player(husuabi_bell_tune, husuabi_bell_beat);
-		//beep(3);
-		//siren(3);
-		//_delay_ms(1000);
-		
-		/*
-		for(int i = 0 ; i < 24; i++)
-		{
-			OCR0A = F_CPU / 2 / 64 /school_bell_tune[i]; //F_CPU / 2 / 분주 / Hz
-			delay_ms(school_bell_beat[i]);
-			OCR0A = 0; //소리 끄기
-			_delay_ms(20);
-		}
-		*/
-	}
-	
-	return 0 ;
-}
 
-void Music_player(int *tune, int *beat)
-{
-	while(*tune != '/0') {
-		if(*tune)OCR1A = F_CPU / 2 / 8 / *tune; //F_CPU / 2 / 분주 / Hz
-		else OCR1A = 0;
-		delay_ms(*beat);
-		tune++;
-		beat++;
-		OCR1A = 0; //소리 끄기
-		_delay_ms(20);
-	}
-	return;
-}
 
 void delay_ms(int ms) //
 {
